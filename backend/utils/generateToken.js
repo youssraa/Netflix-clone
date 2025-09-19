504685ae9270073697992408bda8fc2e9998c942
youssraa <tayachii.yosra@gmail.com>
2025-09-19 21:24:25 +0200

    maxAge secure

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,10 +4,11 @@ import {ENV_VARS} from "../config/envVars.js"
 export const generateTokenAndSetCookies = (userId,res)=>{
     const token = jwt.sign({userId },ENV_VARS.JWT_SECRET ,{expiresIn : "15d"})
     res.cookie("jwt-netflix",token , {
-        maxAge : 15 * 24 * 6 * 1000 ,
+        maxAge :15 * 24 * 60 * 60 * 1000 ,
         httpOnly : true ,
         sameSite : "strict" ,
-        secure :ENV_VARS.ENV_NODE ==! "development"
+        secure : ENV_VARS.ENV_NODE !== "development"
+
 
 
     } )
