11fa2fef0cc57611664a12252c4c0143600cea1e
youssraa <tayachii.yosra@gmail.com>
2025-09-19 21:31:02 +0200

    sameSite none

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,7 +6,7 @@ export const generateTokenAndSetCookies = (userId,res)=>{
     res.cookie("jwt-netflix",token , {
         maxAge :15 * 24 * 60 * 60 * 1000 ,
         httpOnly : true ,
-        sameSite : "strict" ,
+        sameSite : "none" ,
         secure : ENV_VARS.ENV_NODE !== "development"
 
 
